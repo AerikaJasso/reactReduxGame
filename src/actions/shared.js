3c214c7041849e0ccb3ddc5b2ae37cfbb28d4a6b
Ærika <aerikajasso@gmail.com>
2018-09-25 18:13:28 -0700

    Create authedUser Action Export setAuthedUser function from authedUser Action to sheared Action handleInitialData function. handleInitialData function will dispatch setAuthedUser()

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,10 @@
 import { getInitialData } from '../utils/api'
 import { receiveUsers } from '../actions/users'
 import { receiveQuestions } from '../actions/questions'
+import { setAuthedUser } from '../actions/authedUser'
+
+const AUTHED_ID = 'sarahedo'
+//todo: set up authentication replacing string.
 
 
 export function handleInitialData () {
@@ -10,6 +14,8 @@ export function handleInitialData () {
       .then(({ users, questions }) => {
         dispatch(receiveUsers(users))
         dispatch(receiveQuestions(questions))
+        dispatch(setAuthedUser(AUTHED_ID))
+        // sending the dispatches to the reducers to handle them.
       })
   }
 }
