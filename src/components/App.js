5734a3289e4e9e69866f7c20cb75cb2d9b312442
Ærika <aerikajasso@gmail.com>
2018-09-27 18:49:39 -0700

    Create componentDidMount Hook in App Component Dispatch the action creator inside componentDidMount initializing Data. Assure to connect to react-redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
+import React, { Component } from 'react'
+import '../styles/App.css'
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
 class App extends Component {
+  // When app mounts we want to dispatch the action creator
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
     return (
       <div className="App">
@@ -10,5 +16,7 @@ class App extends Component {
     );
   }
 }
-
-export default App;
+/* 
+  we dont need anything from state, the connect invocation can remain blank.
+*/
+export default connect()(App);
