bbcef90165c67c590e322507e1a8b479a00026fe
Ærika <aerikajasso@gmail.com>
2018-10-04 16:05:55 -0700

    Create Login Form Component - Component will be presentational. - Create function to call dispatch.

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import LoginForm from './LoginForm'
+
+//What info do we want to pass from the store to component?
+class Login extends Component {
+  render() {
+    const {userList} = this.props
+    return (
+      <div className='login-container'>
+        <h1>Login</h1>  
+          <LoginForm
+            userList={userList}
+          />    
+      </div>
+    )
+  }
+}
+
+// Ask what state does component need from the Redux Store
+
+// these are just an array of the KEYS.
+function mapStateToProps ({users}){
+  return {
+    userList: Object.keys(users).map((u) => users[u])
+  }
+}
+
+export default connect(mapStateToProps)(Login)
---
+++ src/components/LoginForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+
+class LoginForm extends Component {
+
+  state = {
+    userId:''
+  }
+
+  handleChange = (e) => {
+    const userId = e.target.value
+    this.setState(() => ({
+      userId
+    }))
+  }
+
+  handleLogin = (e) => {
+    e.preventDefault()
+    const {userId} = this.state
+  }
+  render() {
+    
+    console.log("PROPS IN LOGINFORM: ", this.props, "State In LoginForm: ", this.state )
+    const { userList } = this.props
+    const { userId } = this.state
+    return(
+      <div className='login'>
+        <form 
+          className='login-form' 
+          onSubmit={this.handleLogin}
+        >
+          <label>
+            Please select your name: 
+            <select 
+              onChange={this.handleChange}
+            >
+            { userList.map((user) => (
+              <option
+                key={user.id} 
+                value={user.id}
+              >
+                {user.name}
+              </option>
+            ))}
+            </select>
+          </label> 
+          <button
+            className='btn'
+            type='submit'
+          >
+            Enter
+          </button>
+        
+        </form>
+      </div>
+    )
+  }
+}
+
+// What state does the component need from the redux store?
+
+export default connect()(LoginForm)
